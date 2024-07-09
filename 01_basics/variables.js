aa124d44a2ea1bb698466980140ab2581f6880fe
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-07-09 23:52:11 +0530

    9 July(Variables)

---
+++ 01_basics/variables.js
@@ -0,0 +1,11 @@
+const accountId = 12
+let accountName =  "Priyanshu"                    // widely used and modern practice to define variables. (has local scope)
+var accountEmail = "priyanshu@google.com"         // used previously but not in modern JS (ES6) (has global scope)
+accountPassword = "R@shu12"                       // varuables can be defined like this also
+let accountState;
+
+// accountId = 23                                 // not allowed to change constants, rest variables defined by let and var can be changed.
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountName, accountPassword, accountState])        // consople.table is used to display data in tabular form.
