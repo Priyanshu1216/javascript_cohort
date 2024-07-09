7ffb0ffeaacbc5812ef4a147fcaf0a2622ddb5d8
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-07-10 00:11:14 +0530

    9 July(Datatypes)

---
+++ 01_basics/datatype.js
@@ -0,0 +1,15 @@
+// Datatype => Type of data we're dealing 
+
+// number  => 2 to the power 53
+// boolean => true, false
+// null  => itself a value and datatype
+// bigint => big number
+// string => "Priyansu"
+// undefined => something which is not defined
+// symbol =>
+// object => {"key": "value"}
+
+
+// use typeof to see the type of object
+
+console.log(typeof("str"));
