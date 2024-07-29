abbe5a38dc1229eb8b6e947f0f43acf51fb6cec2
Pri-Nigam <priyanshunigam1607@gmail.com>
2024-07-30 01:01:12 +0530

    Object de structuring

---
+++ 02_basics/4_object.js
@@ -0,0 +1,15 @@
+// De-structuring in objects
+
+
+const obj1 = {
+  name: "priyanshu",
+  age: 24,
+  isLoggedIn: false
+}
+
+
+// we can de-structure this object in this way
+
+const {name, age} = obj1;
+
+console.log(name, age);
